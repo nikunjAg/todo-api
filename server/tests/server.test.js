c83fbbc6a5e62dc42ca1a896d970a0e051884b67
nikunjAg <aggarwalnikunj1999@gmail.com>
2018-10-25 11:37:47 +0530

    Test GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+var todos = [{
+    text: 'Test todos text1'
+}, {
+    text: 'Test todos text2'
+}];
+
 beforeEach((done)=> {
-    Todo.remove({}).then(()=> done());
+    Todo.deleteMany({}).then(()=> {
+        Todo.insertMany(todos);
+    }).then(()=> done());
 })
 
 describe('POST /todos', ()=> {
@@ -24,7 +32,7 @@ describe('POST /todos', ()=> {
                 if(err)
                     return done(err);
                 
-                Todo.find().then((todos)=> {
+                Todo.find({text}).then((todos)=> {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -34,6 +42,7 @@ describe('POST /todos', ()=> {
 
     it('should not update the database on wrong entry', (done)=> {
         
+        // here end has a function as the request is async
         request(app)
             .post('/todos')
             .send({})
@@ -43,9 +52,25 @@ describe('POST /todos', ()=> {
                     return done(err);
                 }
                 Todo.find().then((todos)=> {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e)=> done(e));
             });
-    })
+    });
+
+});
+
+describe('GET /todos', ()=> {
+        
+    // here end is normal as the request is not async
+    it('should get all todos', (done)=> {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res)=> {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+
 });
